e352ee9c4766a50a99c2437123bbe0c7b8ef45ec
Robiu-Sani <hafazrobiussani@gmail.com>
2024-08-23 23:41:30 +0600

    varible

--- 1.prientFunction.py
+++ 1.prientFunction.py
@@ -1,4 +1,6 @@
 # ================ first prient 
 # print('hellow world')
 # print('item' * 10) # there this item text will prient 10 time
-print(6546+654654)
+#this is multi line comment 
+''' print(6546+654654) 
+print(6546+654654) '''
---
+++ 2.varivle.py
@@ -0,0 +1,11 @@
+# varivle name is not be able to start by a number
+# varivle is not be able any key word 
+# varivle is must be by one word 
+# python is case sencitive 
+# student = 'any name'
+# print(student)
+
+# data type 
+# 1. string 
+# 2. intizer
+# 3. bolean   (True , False)
